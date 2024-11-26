d1dc97dd4b302c633d806e972d4bd6750cd9dcec
angie-kinya <angiekinya670@gmail.com>
2024-11-26 23:33:29 +0300

    Basic Calculator
    Python program that prompts a user to enter values and the operation they with to perform on the values

---
+++ calculator.py
@@ -0,0 +1,27 @@
+# Basic calculator program
+
+# Get the user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+# Perform the calculation based on the user's input
+if operation == '+':
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/':
+    if num2 != 0: # Check for division by zero
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Error: Invalid operation. Please enter +, -, *, or /")  # If the user enters an invalid operation, print an error message.
+
+# End of program
